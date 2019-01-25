dd9b753ebfc7bbf8dcb3835fb850b5958483bcce
pawluch <krzysztof.pawluch@nsn.com>
2019-01-25 11:22:38 +0100

    show that boost asio can do it

--- test/ut/net/ipv6Tests.cpp
+++ test/ut/net/ipv6Tests.cpp
@@ -4,12 +4,31 @@
 
 #include <gtest/gtest.h>
 #include <net/ipv6.hpp>
+
+#include <boost/asio.hpp>
+
 using namespace ::testing;
+
+TEST(NetTest, boostAsioCanBeAlternative)
+{
+    std::array<uint8_t ,16> expected{};
+    expected = {
+            0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x01
+    };
+    auto ip = boost::asio::ip::address::from_string("::1");
+    if (ip.is_v6()) {
+        auto ipv6 = boost::asio::ip::address_v6::from_string("::1");
+        auto ipv6bytes = ipv6.to_bytes();
+        EXPECT_EQ(expected, ipv6bytes);
+    }
+    else
+        FAIL();
+}
 
 TEST(NetTest, ipv6tobytes)
 {
     std::array<uint8_t ,16> expected{};
-
     expected = {
             0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x00,
             0x00, 0x00, 0x00, 0x00,   0x00, 0x00, 0x00, 0x01
