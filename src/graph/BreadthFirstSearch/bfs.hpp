a45599039059f68d7ed086473b32ad2eaec2fef8
pawluch <krzysztof.pawluch@nsn.com>
2019-02-24 01:00:45 +0100

    sudoku and bfs

--- src/graph/BreadthFirstSearch/bfs.hpp
+++ src/graph/BreadthFirstSearch/bfs.hpp
@@ -7,12 +7,60 @@
 
 #include <graph/Graph.hpp>
 #include <functional>
+#include <queue>
+#include <map>
 
 namespace alg
 {
     namespace graph
     {
         void bfs(const Graph& graph, int src, std::function<void(int)> callback = nullptr);
+
+        template <typename T>
+        void bfs(T src, std::function<void(const std::vector<T>&)> callback = nullptr, std::function<std::vector<T>(const T&)> generator = nullptr)
+        {
+            std::map<T, bool> visited;
+            std::map<T, T> parent;
+            std::queue<T> queue;
+
+            visited[src] = true;
+            queue.push(src);
+            parent[src] = src;
+
+            while (!queue.empty())
+            {
+                auto current = queue.front();
+                queue.pop();
+                if (callback)
+                {
+                    std::vector<T> history;
+                    auto currentCopy = current;
+                    //history.push_back(src);
+                    while (parent[currentCopy] != src)
+                    {
+                        history.push_back(currentCopy);
+                        currentCopy = parent[currentCopy];
+                    }
+                    history.push_back(currentCopy);
+                    history.push_back(src);
+                    std::reverse(history.begin(), history.end());
+                    callback(history);
+                }
+                if (generator)
+                {
+                    auto neighbors = generator(current);
+                    for (auto neighbor: neighbors)
+                    {
+                        if (!visited[neighbor])
+                        {
+                            parent[neighbor] = current;
+                            visited[neighbor] = true;
+                            queue.push(neighbor);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
--- test/ut/graph/BreadthFirstSearch/BreadthFirstSearchTests.cpp
+++ test/ut/graph/BreadthFirstSearch/BreadthFirstSearchTests.cpp
@@ -4,6 +4,7 @@
 
 #include <gtest/gtest.h>
 #include <graph/BreadthFirstSearch/bfs.hpp>
+#include <vector>
 
 using namespace ::testing;
 
@@ -25,4 +26,51 @@ TEST(BFSTest, x)
     alg::graph::bfs(graph, v1, callback);
     std::vector<int> expectedCallsArguments = {v1, v2, v3, v4};
     EXPECT_EQ(expectedCallsArguments, callsArguments);
+}
+
+TEST(BFSTest, RemoveInvalidBrackets)
+{
+    std::vector<std::vector<std::string>> results;
+    auto isValid = [](const std::string& str)->bool
+    {
+        int cnt = 0;
+        for (auto i = 0u; i < str.length(); i++)
+        {
+            std::queue<char> q;
+            if (str[i] == '(')
+                cnt++;
+            else if (str[i] == ')')
+                cnt--;
+            if (cnt < 0)
+                return false;
+        }
+        return (cnt == 0);
+    };
+    std::function<void(const std::vector<std::string>&)> callback = [&](const std::vector<std::string>& str)
+    {
+        if (isValid(str.back()))
+            results.push_back(str);
+    };
+
+    std::function<std::vector<std::string>(const std::string&)> generator = [](const std::string& str)->std::vector<std::string>
+    {
+        std::vector<std::string> result;
+        if (str.length() > 4) {
+            for (auto i = 0u; i < str.length(); i++) {
+                auto copy = str;
+                copy.erase(std::next(copy.begin(), i));
+                result.push_back(copy);
+            }
+        }
+        return result;
+    };
+    std::string str = "()())()";
+    alg::graph::bfs(str, callback, generator);
+
+    std::vector<std::vector<std::string>> expected =  {
+            { "()())()", "(())()" },
+            { "()())()", "()()()" },
+            { "()())()", ")())()", "())()", "()()" },
+            { "()())()", "(())()", "(()()", "(())" } };
+    EXPECT_EQ(expected, results);
 }
